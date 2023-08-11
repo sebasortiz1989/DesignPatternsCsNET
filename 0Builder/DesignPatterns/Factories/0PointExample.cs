7aeab7f882f5bd8f5742797a7e7fe3513b452b23
Sebastian Ortiz S <sebastian@agriculturaverion.com.br>
2023-08-10 21:38:23 -0300

    Factory

--- 0Builder/DesignPatterns/Factories/0PointExample.cs
+++ 0Builder/DesignPatterns/Factories/0PointExample.cs
@@ -10,16 +10,8 @@ namespace Factories
             Polar,
         }
 
-        public class Point
+        public static class PointFactory
         {
-            private double x, y;
-
-            private Point(double x, double y)
-            {
-                this.x = x;
-                this.y = y;
-            }
-
             public static Point NewPointCartesian(double x, double y)
             {
                 return new Point(x, y);
@@ -28,6 +20,17 @@ namespace Factories
             public static Point NewPointPolar(double rho, double theta)
             {
                 return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
+        }
+
+        public class Point
+        {
+            private double x, y;
+
+            public Point(double x, double y)
+            {
+                this.x = x;
+                this.y = y;
             }
 
             public override string ToString()
@@ -38,7 +41,7 @@ namespace Factories
 
         public void Main(string[] args)
         {
-            var point = Point.NewPointPolar(1, Math.PI / 2);
+            var point = PointFactory.NewPointPolar(1, Math.PI / 2);
             Console.WriteLine(point);
         }
     }
